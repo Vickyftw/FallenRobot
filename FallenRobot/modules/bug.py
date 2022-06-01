13c77c59b087527d5934b2e03c0383f9d2bb840b
[ Vicky | JR ] <90129845+Vickyftw@users.noreply.github.com>
2022-06-01 11:16:05 +0530

    Update bug.py

--- FallenRobot/modules/bug.py
+++ FallenRobot/modules/bug.py
@@ -45,7 +45,7 @@ async def bug(_, msg: Message):
     datetimes_fmt = "%d-%m-%Y"
     datetimes = datetime.utcnow().strftime(datetimes_fmt)
 
-    thumb = "https://telegra.ph/file/40eb1ed850cdea274693e.jpg"
+    thumb = "https://telegra.ph/file/4ecc6a0a27b84761ba5a4.jpg"
     
     bug_report = f"""
 **#ʙᴜɢ : ** **@{owner_usn}**
